1c38b2840bf1c1ae2b4fbbdc963389d2f3a9a0c0
littlebigthings-dev <65065269+littlebigthings-dev@users.noreply.github.com>
2022-03-24 16:38:15 +0530

    tricky slider - changes

--- fullpage-slider.js
+++ fullpage-slider.js
@@ -18,119 +18,129 @@ class VERTICALSLIDER {
     checkInView() {
         // function to check scroll up and down + same time enables and disables based on element is in view or not.
         var lastScrollTop = 0;
-        window.addEventListener("scroll", () => { 
-            var st = window.pageYOffset || document.documentElement.scrollTop; 
-            this.isScroll = st > lastScrollTop;
-            lastScrollTop = st <= 0 ? 0 : st; 
-            //    console.log(this.elementIsInView)
-            this.enableDisableSlider()
-        }, false);
+        // window.addEventListener("scroll", () => { 
+        //     var st = window.pageYOffset || document.documentElement.scrollTop; 
+        //     this.isScroll = st > lastScrollTop;
+        //     lastScrollTop = st <= 0 ? 0 : st; 
+        //     // this.enableDisableSlider()
+        // }, false);
         this.observer = new IntersectionObserver((wrapper) => {
             if (wrapper[0]['isIntersecting'] == true) {
                 this.elementIsInView = true;
-                // this.enableDisableSlider(true);
+                console.log('ob - true')
+                // this.removeScroll(true, window);
+                this.centerEle()
+                //    console.log(this.elementIsInView)
+                // setTimeout(() =>{
+                    // this.removeScroll(false, window);
+                // }, 200);
+                // this.enableDisableSlider();
             }
             else{
+                console.log('ob - false')
                 // console.log("not")
                 this.elementIsInView = false;
+                setTimeout( ()=>{
+                    this.swiper.params.mousewheel.releaseOnEdges = false;                
+                }, 200)
                 // this.enableDisableSlider(false);
             }
         }, { root: null, threshold: 0.9, rootMargin: '0px' });
         this.observer.observe(this.itemToObserve);
     }
 
+
+
     enableDisableSlider(){
-
-        function swiperWheelEvents(){
-            
-        }
-
 
         // issues is here when elements comes in view.
         if(this.elementIsInView){
 
 
-            if(this.activeSlide == 0 && this.isScroll && !this.hasSlidesChanged && !this.sliderReset){
+            if(this.activeSlide == 0 && this.isScroll && !this.hasSlidesChanged){
                 // this.swpCrtl.params.mousewheel.releaseOnEdges = false;
                 // console.log("active slide 0, scroll down, slide not changed")
                 // console.log("upar mat jane do")
                 this.swiper.mousewheel.enable();
                 this.sliderReset = true;
-                this.disableScrolling();
+                this.centerEle();
             }
-            else if(this.activeSlide == 0 && !this.isScroll && !this.hasSlidesChanged && this.sliderReset){
+            else if(this.activeSlide == 0 && !this.isScroll && !this.hasSlidesChanged ){
                 // this.swpCrtl.params.mousewheel.releaseOnEdges = false;
                 // console.log("active slide 0, scroll up, slide not changed")
                 // console.log("upar jane do")
                 this.swiper.mousewheel.disable();
                 this.sliderReset = false;
-                this.enableScrolling();
+                this.addScrollFromWindow();
             }
-            else if(this.activeSlide == this.swiper.slides.length-1 && this.isScroll && !this.hasSlidesChanged && this.sliderReset){
+            else if(this.activeSlide == this.swiper.slides.length-1 && this.isScroll && !this.hasSlidesChanged){
                 // console.log("active slide full, scroll down, slide not changed")
                 // console.log("niche jane do")
                 this.hasSlidesChanged = false;
                 this.swiper.mousewheel.disable();
                 this.sliderReset = false;
-                this.enableScrolling();
+                this.addScrollFromWindow();
             }
-            else if(this.activeSlide == this.swiper.slides.length-1 && !this.isScroll && !this.hasSlidesChanged && !this.sliderReset){
+            else if(this.activeSlide == this.swiper.slides.length-1 && !this.isScroll && !this.hasSlidesChanged){
                 // console.log("active slide full, scroll up, slide not changed")
                 // console.log("niche mat jane do")
                 this.swiper.mousewheel.enable();
                 this.sliderReset = true;
-                this.disableScrolling();
+                // this.centerEle();
             }
             // else if(this.activeSlide == this.swiper.slides.length-1 && this.isScroll && !this.hasSlidesChanged && !this.sliderReset){
             //     console.log("active slide full, scroll down, slide not changed")
             //     this.swiper.mousewheel.disable();
             //     this.sliderReset = true;
-            //     this.disableScrolling();
+            //     this.centerEle();
             // }
         }
     }
 
-    removeScroll(flag){
+    removeScroll(flag, ele=window){
         // left: 37, up: 38, right: 39, down: 40,
         // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
         var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
+        
         function preventDefault(e) {
-        e.preventDefault();
+            e.preventDefault();
         }
-
+        
         function preventDefaultForScrollKeys(e) {
-        if (keys[e.keyCode]) {
-            preventDefault(e);
-            return false;
+            if (keys[e.keyCode]) {
+                preventDefault(e);
+                return false;
+            }
         }
-        }
-
+        
         // modern Chrome requires { passive: false } when adding event
         var supportsPassive = false;
         try {
-        window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-            get: function () { supportsPassive = true; } 
-        }));
+            
+            ele.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+                get: function () { supportsPassive = true; } 
+            }));
         } catch(e) {}
-
+        
         var wheelOpt = supportsPassive ? { passive: false } : false;
         var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
+        
         // call this to Disable
         function disableS() {
-        window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-        window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-        window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-        window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+            console.log(ele, "elem in removeScroll Dis");
+            ele.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+            ele.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+            ele.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+            ele.addEventListener('keydown', preventDefaultForScrollKeys, false);
         }
-
+        
         // call this to Enable
         function enableS() {
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-        window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-        window.removeEventListener('touchmove', preventDefault, wheelOpt);
-        window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+            console.log(ele, "elem in removeScroll En");
+            ele.removeEventListener('DOMMouseScroll', preventDefault, false);
+            ele.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+            ele.removeEventListener('touchmove', preventDefault, wheelOpt);
+            ele.removeEventListener('keydown', preventDefaultForScrollKeys, false);
         }        
 
         if(flag == true){
@@ -140,7 +150,7 @@ class VERTICALSLIDER {
         }
     }
 
-    disableScrolling() {
+    centerEle() {
         if (window.screen.width >= 768) {
             // var x = window.scrollX;
             // var y = window.scrollY;
@@ -154,7 +164,7 @@ class VERTICALSLIDER {
             
             window.onscroll =  (event) => { this.itemToObserve.scrollIntoView({
                     block: "center",
-                    behavior: "smooth"
+                    behavior: "auto"
                 });
                 // this.removeScroll(true);
                 // event.preventDefault();
@@ -169,7 +179,7 @@ class VERTICALSLIDER {
     
     
 
-    enableScrolling() {
+    addScrollFromWindow() {
         if (window.screen.width >= 768) {
             window.onscroll = function () { };
             // this.removeScroll(false)
@@ -194,12 +204,12 @@ class VERTICALSLIDER {
                     autoHeight: false,
                     mousewheel: {
                         invert: false,
-                        releaseOnEdges: true,
+                        releaseOnEdges: false,
                         eventsTarget: this.itemToObserve,
                         forceToAxis: true,
                         sensitivity: 0,
                         thresholdDelta: 0,
-                        thresholdTime: 0,
+                        thresholdTime: 200,
                     },
                     pagination: {
                         el: this.pagiItem,
@@ -216,7 +226,16 @@ class VERTICALSLIDER {
                 });
                 this.swiper.on('toEdge', (swiper) => {
                     this.hasSlidesChanged = false;
-                    // this.enableScrolling();
+                    // this.addScrollFromWindow();
+                    this.addScrollFromWindow();
+                    // this.removeScroll(false, window);
+                    setTimeout(() =>{
+                    //         console.log('Check if remove Scroll is called')
+                    //     this.removeScroll(false, window);
+                    this.swiper.params.mousewheel.releaseOnEdges = true;
+                    }, 200);
+                    console.log("reached on edge")
+                    // this.removeScroll(false)
                 })
                 this.swiper.on('slideChange', (swiper) => {
                     this.activeSlide = swiper.activeIndex;
@@ -231,6 +250,7 @@ class VERTICALSLIDER {
 // new VERTICALSLIDER;
 let sliderElm = document.querySelectorAll(".swiper");
 let elemToObserve = document.querySelectorAll("[wrapper='observe']");
+console.log(elemToObserve, "Elem to Obs")
 if(window.screen.width < 768){
     elemToObserve = sliderElm;
 }
